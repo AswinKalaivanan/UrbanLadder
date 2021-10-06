91a7041bbbc4c236b5551c42e51c10441dc3c63e
A P Aswin Kalaivanan <apaswinkalaivanan@Aswins-MacBook-Pro.local>
2021-10-06 15:01:17 +0530

    Version1 second change - kids room webelement and function deleted

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -9,8 +9,7 @@ import org.openqa.selenium.interactions.Actions;
 import javax.swing.*;
 
 public class HomePage {
-    @FindBy(xpath = "(//*[@class='topnav_itemname'])[7]")
-    WebElement kidsroom;
+
 
     @FindBy(xpath = "//*[@class='subnav_item 14423']/a/span")
     WebElement kidsstrogecabinet;
@@ -21,11 +20,7 @@ public class HomePage {
     @FindBy(xpath = "//*[@class='withsubtext']")
     WebElement assertbunkbed;
 
-    public void hoverKidsRoom(WebDriver driver)
-    {
-        Actions act = new Actions(driver);
-        act.moveToElement(kidsroom).perform();
-    }
+
 
     public void assertStorageCabinet()
     {
